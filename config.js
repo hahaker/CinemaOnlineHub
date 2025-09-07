09a56dbca43696ca99fa4ad7de2570146bd3132e
hahaker <hahaker007@gmail.com>
2025-09-07 12:12:23 +0300

    добавил конфиг жиес

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+window.SUPABASE_CONFIG = {
+    URL: 'https://yohribvursfwpbnyshiv.supabase.co',
+    ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlvaHJpYnZ1cnNmd3BibnlzaGl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcyMzIwNTYsImV4cCI6MjA3MjgwODA1Nn0.Spem2pILqlPhUcW770hWiLyRsshLDtNQ_nZJJa3ovyM'
+};
